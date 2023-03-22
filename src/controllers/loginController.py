285a131e725ac52469c9c1e44ea164792f8d78b8
yesid1013 <69687618+yesid1013@users.noreply.github.com>
2023-03-21 23:18:13 -0500

    ajustes del token

--- src/controllers/loginController.py
+++ src/controllers/loginController.py
@@ -20,7 +20,7 @@ def login():
         
             else:
                 access_token = create_access_token(identity=usuario.id_user)                
-                return jsonify(token=access_token)
+                return jsonify({"token" : access_token, "user_id" : usuario.id_user, "cargo" : usuario.cargo})
 
     except Exception as e:
         return jsonify({"Ha ocurrido un error " : str(e) })
